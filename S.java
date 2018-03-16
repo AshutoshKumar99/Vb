b4b5eda1d9dca65a1e0b52de299ed3458895d4c1
Ashutosh Kumar <uniqueraghuvanshi@gmail.com>
2018-03-16 12:05:14 +0530

    Added for loop

--- S.java
+++ S.java
@@ -5,6 +5,17 @@ public static void main(String args[])
 System.out.println("Samsung");
 System.out.println("Micromax");
 
+
+for(int i=1;i<=10;i++)
+{
+	
+	System.out.println("HelloWorld");
+	System.out.println("HelloJava");
+	
+}
+
+
+
 }
 
 
